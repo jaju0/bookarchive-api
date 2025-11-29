39ff7744ee975e52867d94c446e3ea8eb6cf7187
jaju0 <jannes_oltmanns@hotmail.de>
2025-11-29 15:23:34 +0100

    added book model

---
+++ src/models/Book.ts
@@ -0,0 +1,95 @@
+import mongoose from "mongoose";
+import * as langCodes from "../utils/langCodes.js";
+
+export interface IBook extends mongoose.Document
+{
+    title: string;
+    subtitle?: string;
+    description?: string;
+    isbn_10?: string;
+    isbn_13: string;
+    author_id: mongoose.ObjectId;
+    publisher_id: mongoose.ObjectId;
+    publication_date: Date;
+    genre_id: mongoose.ObjectId;
+    language: string;
+    page_count: number;
+    cover_image_url?: string;
+    stock_count: number;
+    createdAt: Date;
+    updatedAt: Date;
+}
+
+type BookModel = mongoose.Model<IBook>;
+
+const bookSchema = new mongoose.Schema<IBook, BookModel>({
+    title: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    subtitle: {
+        type: String,
+        required: false,
+        trim: true,
+    },
+    description: {
+        type: String,
+        required: false,
+        trim: true,
+    },
+    isbn_10: {
+        type: String,
+        required: false,
+        maxLength: 10,
+        trim: true,
+    },
+    isbn_13: {
+        type: String,
+        required: true,
+        maxLength: 13,
+        trim: true,
+    },
+    author_id: {
+        type: mongoose.Types.ObjectId,
+        required: true,
+    },
+    publisher_id: {
+        type: mongoose.Types.ObjectId,
+        required: true,
+    },
+    publication_date: {
+        type: Date,
+        required: true,
+    },
+    genre_id: {
+        type: mongoose.Types.ObjectId,
+        required: true,
+    },
+    language: {
+        type: String,
+        required: true,
+        enum: langCodes.getLangCodes(),
+        trim: true,
+    },
+    page_count: {
+        type: Number,
+        required: true,
+        min: 1,
+    },
+    cover_image_url: {
+        type: String,
+        required: false,
+        trim: true,
+    },
+    stock_count: {
+        type: Number,
+        required: true,
+        min: 0,
+    },
+});
+
+bookSchema.set("timestamps", true);
+
+export const Book = mongoose.model("Book", bookSchema);
+export type Book = InstanceType<typeof Book>;
