36be0e18541fc066f8712d1f5f33c14b6c820c01
jaju0 <jannes_oltmanns@hotmail.de>
2025-11-29 15:24:00 +0100

    added genre model

---
+++ src/models/Genre.ts
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+
+export interface IGenre extends mongoose.Document
+{
+    name: string;
+    description?: string;
+}
+
+type GenreModel = mongoose.Model<IGenre>;
+
+const genreSchema = new mongoose.Schema<IGenre, GenreModel>({
+    name: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    description: {
+        type: String,
+        required: false,
+        trim: true,
+    },
+});
+
+genreSchema.set("timestamps", true);
+
+export const Genre = mongoose.model("Genre", genreSchema);
+export type Genre = InstanceType<typeof Genre>;
