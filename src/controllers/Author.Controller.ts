08e3c3058980dde47c2d2ed6cb1e95b593338e08
jaju0 <jannes_oltmanns@hotmail.de>
2025-12-01 18:01:05 +0100

    implemented author list request handler

--- src/controllers/Author.Controller.ts
+++ src/controllers/Author.Controller.ts
@@ -22,8 +22,8 @@ export interface AuthorListRequestQuery
     last_name?: string;
     birth_date?: Date;
     death_date?: Date;
-    page?: number;
-    page_count?: number;
+    created_at_before?: Date;
+    limit?: number;
 }
 
 export interface AuthorAmendmentRequestBody
@@ -56,6 +56,20 @@ export interface AuthorResponseData
     created_at: Date;
     updated_at: Date;
 }
+
+export interface AuthorListResponseData
+{
+    id: string;
+    first_name: string;
+    last_name: string;
+    birth_date: Date;
+    death_date?: Date;
+    created_at: Date;
+    updated_at: Date;
+}
+
+const defaultPageLimit = process.env.AUTHOR_QUERY_DEFAULT_PAGE_LIMIT ? +process.env.AUTHOR_QUERY_DEFAULT_PAGE_LIMIT : 1000;
+
 
 export const createAuthor = async (req: Request<any, any, AuthorCreationRequestBody>, res: Response<ResponseBody<AuthorCreationResponseData>>) => {
     try
@@ -145,8 +159,53 @@ export const getAuthor = async (req: Request<AuthorRequestParam>, res: Response<
     }
 };
 
-export const getAuthors = (req: Request<any, any, any, AuthorListRequestQuery>, res: Response) => {
-    res.sendStatus(200);
+export const getAuthors = async (req: Request<any, any, any, AuthorListRequestQuery>, res: Response<ResponseBody<AuthorListResponseData[]>>) => {
+    try
+    {
+        const limit = req.query.limit === undefined ? defaultPageLimit : req.query.limit;
+
+        const query = Author.find(undefined, "first_name last_name birth_date death_date createdAt updatedAt", { limit: limit });
+
+        query.find({ createdAt: { $lt: req.query.created_at_before || new Date() } });
+
+        if(req.query.first_name)
+            query.find({ first_name: req.query.first_name });
+        if(req.query.last_name)
+            query.find({ last_name: req.query.last_name });
+        if(req.query.birth_date)
+            query.find({ birth_date: req.query.birth_date });
+        if(req.query.death_date)
+            query.find({ death_date: req.query.death_date });
+
+        const docs = await query.sort("-createdAt").exec();
+
+        if(!docs.length)
+            return res.sendStatus(404);
+
+        const createdAtBefore = docs[docs.length-1].createdAt;
+
+        res.send({
+            data: docs.map(doc => <AuthorListResponseData> {
+                id: doc.id,
+                first_name: doc.first_name,
+                last_name: doc.last_name,
+                birth_date: doc.birth_date,
+                death_date: doc.death_date,
+                created_at: doc.createdAt,
+                updated_at: doc.updatedAt,
+            }),
+            cursor: {
+                created_at_before: createdAtBefore,
+                limit: limit,
+            },
+            errors: [],
+        });
+    }
+    catch(error)
+    {
+        console.error(error);
+        res.sendStatus(500);
+    }
 };
 
 export const deleteAuthor = (req: Request<AuthorRequestParam>, res: Response) => {
