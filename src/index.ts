27b23cac146c24a8dddcf4c9a274c174e26eab61
jaju0 <jannes_oltmanns@hotmail.de>
2025-12-04 15:54:19 +0100

    added db connection setup to index

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import mongoose from "mongoose";
 
 dotenv.config();
+import { connectDB } from "./config/connectDB.js";
 
 const port = process.env.PORT || 3000;
 
@@ -11,6 +13,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.listen(port, () => {
-    console.log(`book-archive api listening on port ${port}`);
+connectDB();
+mongoose.connection.once("open", () => {
+    console.log("book-archive api connected to MongoDB");
+    app.listen(port, () => {
+        console.log(`book-archive api listening on port ${port}`);
+    });
 });
