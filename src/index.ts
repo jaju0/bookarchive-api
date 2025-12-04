e2ccb2c92a43eace5192c8ec5bdc3284e9b58912
jaju0 <jannes_oltmanns@hotmail.de>
2025-12-04 15:54:41 +0100

    added endpoints & rate limiter to express app

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,14 @@ import dotenv from "dotenv";
 import mongoose from "mongoose";
 
 dotenv.config();
+
+import { setupRateLimiter } from "./config/setupRateLimiter.js";
 import { connectDB } from "./config/connectDB.js";
+import { authorsRouter } from "./routes/Author.Route.js";
+import { publisherRouter } from "./routes/Publisher.Route.js";
+import { genreRouter } from "./routes/Genre.Route.js";
+import { all404 } from "./controllers/404.Controller.js";
+import { bookRouter } from "./routes/Book.Route.js";
 
 const port = process.env.PORT || 3000;
 
@@ -12,6 +19,13 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(setupRateLimiter());
+
+app.use("/authors", authorsRouter);
+app.use("/publishers", publisherRouter);
+app.use("/genres", genreRouter);
+app.use("/books", bookRouter);
+app.all("*splat", all404);
 
 connectDB();
 mongoose.connection.once("open", () => {
